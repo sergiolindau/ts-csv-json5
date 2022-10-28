8f6da004d0f4128ba14be72183c00df9e304b40d
Sergio Lindau <sergiolindau@gmail.com>
2022-10-28 11:41:07 -0300

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
 import { Project, StructureKind } from "ts-morph";
+
+const project = new Project({
+    tsConfigFilePath: "testsrc/tsconfig.json",
+});
+
+project.addSourceFilesAtPaths("testsrc/**/*{.d.ts,.ts}");
+
+const sourceFile = project.createSourceFile("testsrc/myStructureFile.ts",
+    {
+        statements: [{
+            kind: StructureKind.Enum,
+            name: "MyEnum",
+            members: [{
+                name: "member",
+            }],
+        }, {
+            kind: StructureKind.Class,
+            name: "MyClass"
+        }],
+    },
+    {
+        overwrite: true
+    }
+);
+
+sourceFile.saveSync();
---
+++ testsrc/myStructureFile.ts
@@ -0,0 +1,6 @@
+enum MyEnum {
+    member
+}
+
+class MyClass {
+}
